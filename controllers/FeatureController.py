1fdbf7ce6ae4d4a04728532c6f445a6d04674fb3
Du4rteRodrigues <93005604+Du4rteRodrigues@users.noreply.github.com>
2022-02-07 18:14:43 +0000

    created add_event feature (#3)
    Co-authored-by: Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -5,10 +5,10 @@ from models.Feature import Feature
 def get_Features():
     #  Intialize your feature here
     greeting = Feature("greet", 0, greet)
-    get_help =  Feature("help", 0, get_help_feature)
-    
+    gethelp =  Feature("!gethelp", 1, get_Help)
+    addevent = Feature("addevent",2,add_event)
     #  Add the initialized feature here
-    features = [greeting, get_help]
+    features = [greeting, gethelp, addevent]
 
     return features
 
@@ -16,8 +16,14 @@ def get_Features():
 async def greet(message):
     await message.channel.send(f"Hi {message.author}! I am Sr.Engenheiro\nWould you like some assistance today?")
     return
+  
 
-
+async def add_event(event_name, event_date, message):
+    events = {}
+    events[event_name] = event_date
+    await message.channel.send(f"I'll add it to the list.")
+    return
+  
 async def get_help_feature(message):
     await message.channel.send(f"Here's a list of all the commands!!\n greet- Be nice and say Hi to Sr.Engenheiro. :)\n help- A list of all the commands.")
     
