7dc37b15bdb1ff7fee70d52775f7c794207fce5c
Francisco Furtado <you@example.com>
2022-02-07 17:44:22 +0000

    help feature

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -1,13 +1,14 @@
+from pyexpat import features
 from models.Feature import Feature
 
 
 def get_Features():
     #  Intialize your feature here
     greeting = Feature("greet", 0, greet)
-    gethelp =  Feature("!gethelp", 1, get_Help)
+    get_help =  Feature("help", 0, get_help_feature)
     
     #  Add the initialized feature here
-    features = [greeting, gethelp]
+    features = [greeting, get_help]
 
     return features
 
@@ -17,5 +18,7 @@ async def greet(message):
     return
 
 
-async def get_Help(message):
+async def get_help_feature(message):
+    await message.channel.send(f"Here's a list of all the commands!!\n greet- Be nice and say Hi to Sr.Engenheiro. :)\n help- A list of all the commands.")
+    
     return
