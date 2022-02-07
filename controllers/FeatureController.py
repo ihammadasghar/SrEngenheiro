67bf2b86b37ac0b7f94e06cd9916b66bd25cf27e
Du4rteRodrigues <duarte.m.rodrigues@sapo.pt>
2022-02-07 18:06:11 +0000

    created add_event feature

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -5,9 +5,9 @@ def get_Features():
     #  Intialize your feature here
     greeting = Feature("greet", 0, greet)
     gethelp =  Feature("!gethelp", 1, get_Help)
-    
+    addevent = Feature("addevent",2,add_event)
     #  Add the initialized feature here
-    features = [greeting, gethelp]
+    features = [greeting, gethelp, addevent]
 
     return features
 
@@ -18,4 +18,10 @@ async def greet(message):
 
 
 async def get_Help(message):
+    return
+
+async def add_event(event_name, event_date, message):
+    events = {}
+    events[event_name] = event_date
+    await message.channel.send(f"I'll add it to the list.")
     return
