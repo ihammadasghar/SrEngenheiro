bd2d4a4a6260f26af0544ede857b4863d32dd52f
Hammad Asghar <ihammadasghar@gmail.com>
2022-02-09 21:38:30 +0000

    Change Record function return type to list and optimize code

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -45,66 +45,55 @@ class FeatureController:
         return
 
 
-    def sort_dates(self,events):
-        sorted_events = {}
-        events_list = []
-        for event in events.items():
-            events_list.append({"name": event[0], "date": event[1]})
-        events_list.sort(key=lambda x: datetime.datetime.strptime(x['date'], '%d/%m/%Y'))
-
-        for event in events_list:
-            sorted_events.update({event["name"]:event["date"]})
-
-        return sorted_events
-
-
     async def event(self, action, topic, name=None, date=None):
         action = action.upper()
         topic = topic.upper()
 
         if action == "ADD":
             name = name.upper()
-            record = {name: date}
+            record = [{"name": name, "item": date}]
             await self.records.add(topic=topic, records=record)
-            await self.message.channel.send(f" I'll add {name} to {topic}.")
+            await self.message.channel.send(f"{name} added to {topic}.")
             return
-
+        
         elif action == "GET":
             records = self.records.get(topic=topic)
-            records = self.sort_dates(records)
+            records.sort(key=lambda x: datetime.datetime.strptime(x['item'], '%d/%m/%Y'))
 
             #  In case topic doesnt exist
             if records is None:
-                await self.message.channel.send(f"Sorry, I have no records of the event {topic}")
+                await self.message.channel.send(f"Sorry, I have no records of the topic {topic}")
                 return
 
             if not name is None:
                 name = name.upper()
 
-                #  In case the name doesnt exist in the topic
-                try:
-                    record = records[name]
-                except KeyError:
-                    await self.message.channel.send(f"Sorry, I have no record of {name} in the event {topic}.")
-                    return
-
-                await self.message.channel.send(f"{topic}: {name} - {record}.")
+                for record in records:
+                    if record["name"] == name:
+                        await self.message.channel.send(f"{topic}: {name} - {record}.")
+                        return
+                #  Record not found
+                await self.message.channel.send(f"Sorry, I have no record of {name} in the topic {topic}.")
                 return
 
             result = topic
-            for record in records.items():
-                result += "\n" + record[0] + " " + record[1]
+            for record in records:
+                result += "\n-> " + record["name"] + " " + record["item"]
 
             await self.message.channel.send(result)
             return
 
+
         elif action == "DELETE":
+            if name is None:
+                await self.message.channel.send(f"Missing event name.")
+                return
             name = name.upper()
-            is_removed = await self.records.remove(topic, record_Key=name)
-            if not is_removed:
+            is_Removed = await self.records.remove(topic, record_Name=name)
+            if not is_Removed:
                 await self.message.channel.send(f"No such record exists.")
                 return
-            await self.message.channel.send(f" I'll remove {name} from {topic}.")
+            await self.message.channel.send(f"{name} removed from {topic}.")
             return
         
         else:
@@ -129,7 +118,7 @@ class FeatureController:
 
         if action == "ADD":
             name = name.upper()
-            record = {name: item}
+            record = [{"name": name, "item": item}]
             await self.records.add(topic=topic, records=record)
             await self.message.channel.send(f"{name} added to {topic}.")
             return
@@ -144,19 +133,17 @@ class FeatureController:
             if not name is None:
                 name = name.upper()
 
-                #  In case the name doesnt exist in the topic
-                try:
-                    record = records[name]
-                except KeyError:
-                    await self.message.channel.send(f"Sorry, I have no record of {name} in the topic {topic}.")
-                    return
-
-                await self.message.channel.send(f"{topic}: {name} - {record}.")
+                for record in records:
+                    if record["name"] == name:
+                        await self.message.channel.send(f"{topic}: {name} - {record}.")
+                        return
+                #  Record not found
+                await self.message.channel.send(f"Sorry, I have no record of {name} in the topic {topic}.")
                 return
 
             result = topic
-            for record in records.items():
-                result += "\n" + record[0] + " " + record[1]
+            for record in records:
+                result += "\n-> " + record["name"] + " " + record["item"]
 
             await self.message.channel.send(result)
             return
@@ -164,8 +151,8 @@ class FeatureController:
 
         elif action == "DELETE":
             name = name.upper()
-            is_removed = await self.records.remove(topic, record_Key=name)
-            if not is_removed:
+            is_Removed = await self.records.remove(topic, record_Name=name)
+            if not is_Removed:
                 await self.message.channel.send(f"No such record exists.")
                 return
             await self.message.channel.send(f"{name} removed from {topic}.")
--- models/Records.py
+++ models/Records.py
@@ -21,53 +21,48 @@ class Records:
         
         record = record_Message.content.split("\n")[1:]
 
-        record_dict = {}
+        records_List = []
         for item in record:
             item = item.split(" ")
-            record_dict.update({item[0]: item[1]})
+            records_List.append({"name": item[0], "item": item[1]})
         
-        return record_dict
+        return records_List
 
 
     async def update(self, topic, records):
         record_Message = self.get_Message(topic)
         new_Record = f"{topic}"
 
-        for item in records.items():
-                new_Record += "\n" + item[0] + " " + item[1]
+        for record in records:
+                new_Record += "\n" + record["name"] + " " + record["item"]
         
+        if record_Message is None:
+            await self.data_Channel.send(new_Record)
+            return
+
         await record_Message.edit(content=new_Record)
         return
 
 
     async def add(self, topic, records):
-        record_Message = self.get_Message(topic)
-
-        #  If no previous record of the record type
-        if record_Message is None:
-            result = topic
-            for record in records.items():
-                result += "\n" + record[0] + " " + record[1]
-            await self.data_Channel.send(result)
-            return
-        
-        old_records = self.get(topic)
-        old_records.update(records)
-        await self.update(topic, old_records)
+        old_Records = self.get(topic)
+        if not old_Records is None:
+            records = old_Records + records
+        await self.update(topic, records)
 
     
 
-    async def remove(self, topic, record_Key):
+    async def remove(self, topic, record_Name):
         records = self.get(topic)
 
         #  If record doesn't exist
         if not records:
             return False
         
-        try:
-            records.pop(record_Key)
-        except KeyError:
-            return False
-
-        await self.update(topic, records)
-        return True
+        for record in records:
+            if record["name"] == record_Name:
+                records.remove(record)
+                await self.update(topic, records)
+                return True
+        #  Record not found
+        return False
