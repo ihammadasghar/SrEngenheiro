701edb4cc3dd924beca30b5343c16becf47c1d67
Du4rteRodrigues <93005604+Du4rteRodrigues@users.noreply.github.com>
2022-02-10 13:32:52 +0000

    Current day (#7)
    * added the feature to add, get, sort and delete events
    
    * current day feature
    
    * Cleaned code
    
    1.Its a good practice to have 2 empty lines after each function.
    2.Changed GOOD_BOT to GOOD-BOT
    3.Always a good idea to choose more descriptive variable name (instead of d1, used date_Today)
    
    Co-authored-by: Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -1,5 +1,6 @@
 from models.Feature import Feature
 import datetime
+from datetime import date
 
 class FeatureController:
     def __init__(self, message, server) -> None:
@@ -15,8 +16,20 @@ class FeatureController:
                                     functionality=self.greet, 
                                     description="Hi: greetings!"
                                     )
+        
+        current_day_feature = Feature(command="TODAY",
+                                    args=0,
+                                    functionality=self.today,
+                                    description="TODAY: Tells the current date."
+                                    )               
 
-        get_help_feature =  Feature(command="HELP", 
+        praise_feature = Feature(command="GOOD-BOT",
+                                    args=0, 
+                                    functionality=self.praise,
+                                    description="PRAISE: ???."
+                                    )
+            
+        get_help_feature = Feature(command="HELP", 
                                     args=0, 
                                     functionality=self.get_help,
                                     description="HELP: Lists all commands."
@@ -35,7 +48,8 @@ class FeatureController:
                                 )
 
         #  Add the initialized feature here
-        features = [greeting_feature, get_help_feature, event_feature, note_feature]
+
+        features = [greeting_feature, current_day_feature, praise_feature, get_help_feature, event_feature, note_feature]
 
         return features
 
@@ -44,6 +58,18 @@ class FeatureController:
         await self.message.channel.send(f"Hi {self.message.author.nick}! Sr.Engenheiro here\n-> sr! help: To see how I can help")
         return
 
+    
+    async def today(self):
+        date_Today = date.today()
+        date_Today = today.strftime("%d/%m/%Y")
+        await self.message.channel.send(f"Today's date is {date_Today}")
+        return
+
+    
+    async def praise(self):
+        await self.message.channel.send(f"(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
+        return
+ 
 
     async def event(self, action, topic, name=None, date=None):
         action = action.upper()
@@ -97,6 +123,73 @@ class FeatureController:
                 await self.message.channel.send(f"No such record exists.")
                 return
             await self.message.channel.send(f"{name} removed from {topic}.")
+            return
+        
+        else:
+            await self.message.channel.send(f"I don't know how to perform action {action} :(")
+            return
+
+
+    def sort_dates(self,events):
+        sorted_events = {}
+        events_list = []
+        for event in events.items():
+            events_list.append({"name": event[0], "date": event[1]})
+        events_list.sort(key=lambda x: datetime.datetime.strptime(x['date'], '%d/%m/%Y'))
+
+        for event in events_list:
+            sorted_events.update({event["name"]:event["date"]})
+
+        return sorted_events
+
+
+    async def event(self, action, topic, name=None, date=None):
+        action = action.upper()
+        topic = topic.upper()
+
+        if action == "ADD":
+            name = name.upper()
+            record = {name: date}
+            await self.records.add(topic=topic, records=record)
+            await self.message.channel.send(f" I'll add {name} to {topic}.")
+            return
+
+        elif action == "GET":
+            records = self.records.get(topic=topic)
+            records = self.sort_dates(records)
+
+            #  In case topic doesnt exist
+            if records is None:
+                await self.message.channel.send(f"Sorry, I have no records of the event {topic}")
+                return
+
+            if not name is None:
+                name = name.upper()
+
+                #  In case the name doesnt exist in the topic
+                try:
+                    record = records[name]
+                except KeyError:
+                    await self.message.channel.send(f"Sorry, I have no record of {name} in the event {topic}.")
+                    return
+
+                await self.message.channel.send(f"{topic}: {name} - {record}.")
+                return
+
+            result = topic
+            for record in records.items():
+                result += "\n" + record[0] + " " + record[1]
+
+            await self.message.channel.send(result)
+            return
+
+        elif action == "DELETE":
+            name = name.upper()
+            is_removed = await self.records.remove(topic, record_Key=name)
+            if not is_removed:
+                await self.message.channel.send(f"No such record exists.")
+                return
+            await self.message.channel.send(f" I'll remove {name} from {topic}.")
             return
         
         else:
