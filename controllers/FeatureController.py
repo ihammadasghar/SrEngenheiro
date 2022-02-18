2680852bfb75b90ac16085be024c0ca3d607cf15
Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>
2022-02-18 12:31:09 +0000

    Ordered events command (#19)
    * Add ordered events command
    
    * Delete expired events funtion

--- controllers/FeatureController.py
+++ controllers/FeatureController.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from msilib.schema import tables
 
 
 def get_Date_Today():
@@ -61,6 +62,48 @@ def delete_Notes_Topic(records, topic):
     deleted = records.remove(table="NOTES", topic=topic)
     return deleted
 
+#  Events
+def get_Ordered_Events(records):
+    delete_Expired_Events(records)
+
+    events = records.get(table="EVENTS")
+
+    if events is None:
+        return None
+
+    #  Events across all topics
+    all_events = []
+    for topic in events.keys():
+        topic_events = [{"Topic": topic,"Name": name, "Date": events[topic][name]} for name in events[topic].keys()]
+        all_events += topic_events
+
+    all_events.sort(key=lambda x: datetime.strptime(x["Date"], '%d/%m/%Y'))
+    return all_events
+
+
+def delete_Expired_Events(records):
+    events = records.get(table="EVENTS")
+    expired_events = []
+    for topic in events.keys():
+        for name in events[topic].keys():
+            days_Left = get_Days_Left(events[topic][name])
+            if days_Left<0:
+                expired_events.append((topic, name))
+    
+    #  Removing expired events
+    for event in expired_events:
+        records.remove(table="EVENTS", topic=event[0], name=event[1])
+
+
+def get_Days_Left(event_Date):
+    date_Today = get_Date_Today().split("/")
+    date_Today = list(map(int, date_Today))
+    date_Today = date(date_Today[2], date_Today[1], date_Today[0])
+    event_Date = event_Date.split("/")
+    event_Date = list(map(int, event_Date))
+    event_Date = date(event_Date[2], event_Date[1], event_Date[0])
+    return (event_Date-date_Today).days
+
 
 def edit_Event(topic, name, date, records):
     edited = records.update(table="EVENTS", topic=topic, name=name, item=date)
@@ -73,6 +116,7 @@ def add_Event(topic, name, date, records):
 
 
 def get_Event(records, topic, name):
+    delete_Expired_Events(records)
     event = records.get(table="EVENTS", topic=topic)
     if not event:
         return None
@@ -103,10 +147,7 @@ def get_Events_Topic(records, topic):
 
 
 def urgent_Events(records):
-    date_Today = get_Date_Today().split("/")
-    date_Today = list(map(int, date_Today))
-    date_Today = date(date_Today[2], date_Today[1], date_Today[0])
-
+    delete_Expired_Events(records)
     table = records.get(table="EVENTS")
     events = []
     for topic in table.keys():
@@ -121,10 +162,7 @@ def urgent_Events(records):
     
     text = f"**Urgent events:**\n"
     for event in events:
-        event_Date = event[1].split("/")
-        event_Date = list(map(int, event_Date))
-        event_Date = date(event_Date[2], event_Date[1], event_Date[0])
-        days_Left = (event_Date-date_Today).days
+        days_Left = get_Days_Left(event[1])
         if  7>= days_Left:
             if days_Left == 1:
                 text += f"**-> {event[0]}** `Is due today!`\n" 
