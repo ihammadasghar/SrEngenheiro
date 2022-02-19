80e27cd00e905fbdda48cf556de88a8462d68f51
Hammad Asghar <ihammadasghar@gmail.com>
2022-02-19 19:31:45 +0000

    Fix feature descriptions error

--- features_Config.py
+++ features_Config.py
@@ -4,40 +4,41 @@ from views.response import *
 #  Intialize your feature here
 greeting_feature =  Feature(command="HI", 
                             view_Function=greet, 
-                            description="**-> Hi**\n```sr! hi```",
+                            description="**-> Hi**: `sr! hi`",
                             message_Required=True
                             )
 
 current_day_feature = Feature(command="TODAY",
                             view_Function=today,
-                            description="**-> Date today**\n```sr! today```"
+                            description="**-> Date today**: `sr! today`"
                             )               
 
 praise_feature = Feature(command="GOOD-BOT",
                             view_Function=praise,
-                            description="**-> Praise**\n```sr! Good-Bot```"
+                            description="**-> Praise**: `sr! Good-Bot`"
                             )
 
 scold_feature = Feature(command="BAD-BOT",
                             view_Function=scold,
-                            description="**-> Scold**\n```sr! Bad-Bot```"
+                            description="**-> Scold**: `sr! Bad-Bot`"
                             )
 
     
 help_feature = Feature(command="HELP", 
                             view_Function=help,
-                            description="**-> Help**\n```sr help```"
+                            description="**-> Help**: `sr help`"
                             )
 
-events_description ="""**-> Events**
-```sr! events [add/edit/get/delete] [topic] [name] [Day/Month/Year]
-sr! events Add Tests Math 22/4/2022```
--> Events in the next 7 days
-```sr! events urgent```
-Adding multiple events
-```sr! events Add Tests
-Math 22/4/2022
-English 21/4/2022```"""
+events_description ="""**-> Events:**
+ - Schedule: `sr! events`
+ - Urgent: `sr! events urgent`
+ - Topics: `sr! events topics`
+ - Add/Edit:`sr! events [add/edit] [topic] [name] [Day/Month/Year]`
+ - Multiple events:
+```sr! events [add/edit] [topic]
+[name] [Day/Month/Year]
+[name] [Day/Month/Year]```
+ - Get/Delete: `sr! events [get/delete] [topic] [name(optional)]`"""
 events_feature = Feature(command="EVENTS", 
                             args_required=True, 
                             view_Function=events, 
@@ -45,13 +46,14 @@ events_feature = Feature(command="EVENTS",
                             description=events_description
                           )
 
-notes_description = """**-> Notes**
-```sr! notes [add/edit/get/delete] [topic] [name] [item]
-sr! notes add Gods Thor "The strongest!"```
-Adding multiple notes
-```sr! notes add Gods
-Zeus "Thunder innit"
-Aries "The god of war"```"""
+notes_description = """**-> Notes:**
+ - Topics: `sr! notes` 
+ - Add/Edit: `sr! notes [add/edit] [topic] [name] [item]`
+ - Multiple notes:
+```sr! notes [add/edit] [topic]
+[name] "[content]"
+[name] "[content]"```
+ - Get/Delete: `sr! notes [get/delete] [topic] [name(optional)]`"""
 notes_feature = Feature(command="NOTES", 
                         args_required=True, 
                         view_Function=notes,
@@ -64,21 +66,21 @@ remember_feature = Feature(command="REMEMBER",
                             records_Required=True,
                             message_Required=True,
                             view_Function=remember_Message,
-                            description="**-> Remember messages/files**:\nReply this command to a message/file:\n```sr! remember [name]```"
+                            description="**-> Remember messages/files**: `sr! remember [name]`(reply to the message)"
                             )
 
 forget_feature = Feature(command="FORGET",
                             args_required=True,
                             records_Required=True,
                             view_Function=forget_Message,
-                            description="**-> Remember messages/files**:\nReply this command to a message/file:\n```sr! remember [name]```"
+                            description="**-> Forget messages/files**: `sr! forget [name]`"
                             )
 
 get_message_feature = Feature(command="GET",
                             args_required=True,
                             records_Required=True,
                             view_Function=get_Message,
-                            description="**-> Remember messages/files**:\nReply this command to a message/file:\n```sr! remember [name]```"
+                            description="**-> Get messages/files**: `sr! get [name]`"
                             )
 
 #  Add the initialized feature here
