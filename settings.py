e1065d67ad0a65a723a0efeb8b46e132bb06dc73
Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>
2022-02-19 13:34:27 +0000

    Clean up code (#22)
    * Add persistence controller to better organize
    
    * Clean response view code
    
    * Add settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#  Environment variables
+TOKEN = os.getenv("TOKEN")
+DATA_CHANNEL_ID = int(os.getenv("DATA_CHANNEL_ID"))
+
+#  Bot settings
+ACTIVATION_WORD = "sr!"
+STARTUP_MESSAGE = "No Fear! Sr.Engenheiro here!"
+MAX_RECORD_FILES = 500
--- views/response.py
+++ views/response.py
@@ -1,57 +1,66 @@
 from controllers import FeatureController as fclr
 from controllers import ValidationController as vclr
+from controllers import PersistenceController as pclr
 
 
-async def main(message, features, records):
-    if message.content.startswith("sr!"):
-        commands = vclr.get_Commands(message.content)
-        command = commands[1].upper()
+async def main(message, features, server):
+    #  Getting server records
+    records = await pclr.load_Records(server)
+    
+    commands = vclr.get_Commands(message.content)
+    command = commands[1].upper()
 
-        #  Special feature cases
-        if command == "HELP":  #  Case: Requires all feature list
-            response = help(features)
-            await message.channel.send(response)
-            return
-
-        for feature in features:
-            if command == feature.command:
-                params = vclr.get_Args(commands, feature, records, message)
-                response = feature.view_Function(*params)
-                await message.channel.send(response)
-                return
-
-        await message.channel.send(f"Sorry, I dont understand this command :/")
+    #  Special feature cases
+    if command == "HELP":  #  Case: Requires all feature list
+        response = help(features)
+        await message.channel.send(response)
         return
+
+    #  Identify feature to execute
+    for feature in features:
+        if command == feature.command:
+            params = vclr.get_Args(commands, feature, records, message)
+            response = feature.view_Function(*params)
+            await message.channel.send(response)
+
+            #  Update Records
+            if records.updated:
+                await pclr.save_Records(records.records, server)
+            
+            # If user has requested to fetch a message
+            id = records.requested_message_ID if records.requested_message_ID else None
+            return id
+
+    await message.channel.send(f"Sorry, I dont understand this command :/")
+    return
 
 
 def greet(message):
     name = message.author.display_name
-    response = f"Hi {name}! `sr! help` to see how I can help"
-    return response
+    return f"Hi {name}! `sr! help` to see how I can help"
 
 
 def today():
     date_Today = fclr.get_Date_Today()
-    response = f"Date today is `{date_Today}`"
-    return response
+    return f"Date today is `{date_Today}`"
 
-
+#  Easter Egg
 def praise():
-    response = "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
-    return response
+    return "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
 
-
+#  Easter Egg
 def scold():
-    response = "ಥ_ಥ"
-    return response
+    return "ಥ_ಥ"
 
-
+#  Remembering messages features
 def remember_Message(args, records, message):
+    #  Argument Validations
     if len(args) != 1 or type(args[0]) == list:
         return "I don't understand :/\nCorrect command to remember:\n`sr! remember [name]`"
     
     if message.reference.message_id == None:
         return "You need to reply this command to the message I need to remember."
+
 
     name = args[0]
     added = fclr.add_Message(name, message.reference.message_id, records)
@@ -62,18 +71,20 @@ def remember_Message(args, records, message):
 
 
 def get_Message(args, records):
+    #  Argument Validation
     if len(args) != 1 or type(args[0]) == list:
         return "I don't understand :/\nCorrect command to get remembered message:\n`sr! get [name]`"
     
     name = args[0]
     found = fclr.get_Message(name, records)
     if found:
-        return f"Remembering **{name}**..."
+        return f"Searching through memories for **{name}**..."
 
     return f"I don't remember the message **{name}**"
 
 
 def forget_Message(args, records):
+    #  Argument Validation
     if len(args) != 1:
         return "I don't understand :/\nCorrect command to forget:\n`sr! forget [name]`"
     
@@ -88,12 +99,13 @@ def forget_Message(args, records):
 def notes(args, records):
     action =  args[0].upper()
     if action == "ADD":
+        #  Argument Validations
         if len(args) != 4 and len(args) != 3:
-            response = "I don't understand :/\nCorrect command to add note:\n`sr! notes add [topic] [name] [content]`"
-            return response
+            return "I don't understand :/\nCorrect command to add note:\n`sr! notes add [topic] [name] [content]`"
 
         topic = args[1].upper()
         
+        #  Multiple note entries
         if type(args[2]) == list:
             entries = args[2]
             names = ""
@@ -102,24 +114,25 @@ def notes(args, records):
                 name = entry[0].upper()
                 names += name + ", "
                 added = fclr.add_Note(records=records, topic=topic, name=name, item=entry[1])
-            if added:
-                response = f"Noted **{names}** in topic **{topic}**."
-                return response
-            response = f"One or more of notes **{names}** already exists in topic **{topic}**."
-            return response
 
+            if added:
+                return f"Noted **{names}** in topic **{topic}**."
+
+            return f"One or more of notes **{names}** already exists in topic **{topic}**."
+
+        #  Single note entry
         name = args[2].upper()
         item = args[3].upper()
         added = fclr.add_Note(records=records, topic=topic, name=name, item=item)
 
         if added:
-            response = f"Noted **{name}** in topic **{topic}**."
-            return response
-        response = f"A note with the name **{name}** already exists in topic **{topic}**."
-        return response
+            return f"Noted **{name}** in topic **{topic}**."
+
+        return f"A note with the name **{name}** already exists in topic **{topic}**."
 
 
     elif action == "DELETE":
+        # Argument Validations
         if len(args) != 3 and len(args) != 2:
             response = "I don't understand :/\nCorrect command to delete note:\n`sr! notes delete [topic] [name(optional)]`"
             return response
@@ -127,16 +140,14 @@ def notes(args, records):
         topic = args[1].upper()
         if len(args) == 3:
             name = args[2].upper()
-            is_Deleted = fclr.delete_Note(topic=topic, name=name, records=records)
-            if is_Deleted:
-                response = f"Note **{name}** deleted from topic **{topic}**."
-                return response
+            deleted = fclr.delete_Note(topic=topic, name=name, records=records)
+            if deleted:
+                return f"Note **{name}** deleted from topic **{topic}**."
 
-            response = f"Couldn't find note **{name}**."
-            return response
+            return f"Couldn't find note **{name}**."
 
-        is_Deleted = fclr.delete_Notes_Topic(topic=topic, records=records)
-        if is_Deleted:
+        deleted = fclr.delete_Notes_Topic(topic=topic, records=records)
+        if deleted:
                 response = f"Notes topic **{topic}** deleted."
                 return response
         response = f"Couldn't find notes topic **{topic}**."
@@ -144,11 +155,12 @@ def notes(args, records):
 
 
     elif action == "GET":
+        #  Argument Validations
         if len(args) != 3 and len(args) != 2 and len(args) != 1:
             response = "I don't understand :/\n Correct command to get note:\n`sr! notes get [topic] [name(optional)]`"
             return response
 
-        #  Get note topic names
+        #  Topic names
         if len(args) == 1:
             topic_names = fclr.get_Notes(records)
             if topic_names is None:
@@ -160,6 +172,7 @@ def notes(args, records):
             
             return response
 
+        #  Single Note
         topic = args[1].upper()
         if len(args) == 3:
             name = args[2].upper()
@@ -168,6 +181,7 @@ def notes(args, records):
                 return f"Couldn't find note **{name}**."
             return note
 
+        # Note topic
         topic_Notes = fclr.get_Notes_Topic(topic=topic, records=records)
         if not topic_Notes:
             return f"Couldn't find notes on topic **{topic}**."
@@ -175,6 +189,7 @@ def notes(args, records):
 
 
     elif action == "EDIT":
+        #  Argument Validations
         if len(args) != 4 and len(args) != 3:
             response = "I don't understand :/\n Correct command to edit note:\n`sr! notes edit [topic] [name] [content]`"
             return response
@@ -211,6 +226,8 @@ def notes(args, records):
 
 
 def events(args, records):
+    # Listing Ordered Events
+    #  Argument Validations
     if len(args) == 0:
         all_events = fclr.get_Ordered_Events(records)
         if all_events is None:
@@ -221,8 +238,10 @@ def events(args, records):
             response += f"-> **({event['Topic']})** {event['Name']} - `{event['Date']}`\n"
         return response
 
+    # CRUD Actions
     action =  args[0].upper()
     if action == "ADD":
+        #  Argument Validations
         if len(args) != 4 and len(args) != 3:
             response = "I don't understand :/\nCorrect command to add event:\n`sr! events add [topic] [name] [date]`"
             return response
@@ -261,34 +280,38 @@ def events(args, records):
 
 
     elif action == "DELETE":
+        #  Argument Validations
         if len(args) != 3 and len(args) != 2:
             response = "I don't understand :/\n Correct command to delete event:\n`sr! events delete [topic] [name(Optional)]`"
             return response
 
         topic = args[1].upper()
+        #  Argument Validations
         if len(args) == 3:
             name = args[2].upper()
-            is_Deleted = fclr.delete_Event(topic=topic, name=name, records=records)
-            if is_Deleted:
+            deleted = fclr.delete_Event(topic=topic, name=name, records=records)
+            if deleted:
                 response = f"Event **{name}** deleted from topic **{topic}**."
                 return response
 
             response = f"Couldn't find event **{name}**."
             return response
 
-        is_Deleted = fclr.delete_Events_Topic(topic=topic, records=records)
-        if is_Deleted:
+        deleted = fclr.delete_Events_Topic(topic=topic, records=records)
+        if deleted:
                 response = f"Events topic **{topic}** deleted."
                 return response
         response = f"Couldn't find events topic **{topic}**."
         return response
     
     elif action == "GET":
+        #  Argument Validations
         if len(args) != 3 and len(args) != 2 and len(args) != 1:
             response = "I don't understand :/\n Correct command to get event:\n`sr! events get [topic] [name(Optional)]`"
             return response
 
         #  Get note topic names
+        #  Argument Validations
         if len(args) == 1:
             topic_names = fclr.get_Events(records)
             if topic_names is None:
@@ -301,6 +324,7 @@ def events(args, records):
             return response
 
         topic = args[1].upper()
+        #  Argument Validations
         if len(args) == 3:
             name = args[2].upper()
             event = fclr.get_Event(topic=topic, name=name, records=records)
@@ -322,6 +346,7 @@ def events(args, records):
     
 
     elif action == "EDIT":
+        #  Argument Validations
         if len(args) != 4 and len(args) != 3:
             response = "I don't understand :/\n Correct command to edit events:\n`sr! events edit [topic] [name] [date]`"
             return response
